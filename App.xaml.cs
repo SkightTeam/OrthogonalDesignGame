870110e77800b74eb688b139ca196ae91ed8622c
Hao Wang <HWonner@gmail.com>
2013-09-30 13:20:37 -0600

    Add 1st version of game

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+
+namespace Driver {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Driver {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() 
+        {
+            InitializeComponent();
+            canvas1.Focus();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e) 
+        {
+            switch (e.Key) {
+                case Key.Up:
+                    Canvas.SetTop(car, Canvas.GetTop(car) - 10);
+                    break;
+                case Key.Down:
+                    Canvas.SetTop(car, Canvas.GetTop(car) + 10);
+                    break;
+
+                case Key.Left:
+                    Canvas.SetLeft(car, Canvas.GetLeft(car) - 10);
+                    break;
+                case Key.Right:
+                    Canvas.SetLeft(car, Canvas.GetLeft(car) + 10);
+                    break;
+            }
+        }
+    }
+}
